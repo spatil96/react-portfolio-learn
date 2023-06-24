c3bb4010b2f0cfa6f3a198626628953a80c594a2
Sumeet Patil <spatil0296@gmail.com>
2023-06-24 11:12:53 -0400

    finalizing the styling and recfactoring the code

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
+import { Routes, Route } from 'react-router-dom';
+import Layout from './components/Layout';
 import './App.scss';
 
 function App() {
   return (
     <>
     <Routes>
-    <Route path = "/" element={<Layout />}
+    <Route path = "/" element={<Layout />} />
     
     </Routes>
     </>
---
+++ src/components/Layout/index.js
@@ -0,0 +1,5 @@
+import './index.scss'
+const Layout = () => {
+    return <h1>Hello</h1>
+}
+export default Layout
