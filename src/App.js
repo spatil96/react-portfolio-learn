68c071708e568ba64b7260c0852f29e01ecf60d1
Sumeet Patil <spatil0296@gmail.com>
2023-06-25 14:31:57 -0400

    Homepage intermediate progress

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import { Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
 import './App.scss';
 import Home from './components/Home';
+import Sidebar from './components/Sidebar';
 
 function App() {
   return (
     <>
     <Routes>
+    
     <Route path = "/" element={<Layout />} />
     <Route index element = {<Home/>} />
     </Routes>
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,20 +1,42 @@
 import { Link } from 'react-router-dom'
 import LogoTitle from '../../assets/images/logo-s.png'
-import './index.scss'
+import '../Home/Logo/index.scss'
+import { useState } from 'react'
+import AnimatedLetters from '../AnimatedLetters'
+import Logo from './Logo'
 const Home = () => {
+    const [letterClass, setLetterClass] = useState('text-animate')
+    const nameArray = ['u', 'm', 'e', 'e', 't']
+    const jobArray = ['F', 'u']
     return (
         <div className="container home-page">
             <div className="text-zone">
-                <h1>Hi, <br/>
+                <h1>
+                    <span className={letterClass}>H</span>
+                    <span className={`${letterClass} _12`}>i,</span>
+                     <br />
                     I'm
-                <img src = {LogoTitle} alt="developer" />
-                umeet
-                <br/>
-                Full Stack developer
+                    <span className={`${letterClass} _13`}>I',</span>
+                    <span className={`${letterClass} _14`}>m</span>
+                    <img src={LogoTitle} alt="developer" />
+                    <AnimatedLetters letterClass={letterClass}
+                        strArray={nameArray}
+                        idx={15} />
+                    <br />
+                    <AnimatedLetters
+                        letterClass={letterClass}
+                        strArray={jobArray}
+                        idx={22}
+                    />
                 </h1>
                 <h2>Spring Boot/ Java 8/ React Js/ JavaScript/ SQL</h2>
-                <Link to="/contact" className="flat-button">Contact Me</Link>
+                <Link 
+                    to="/contact" 
+                    className="flat-button">
+                        Contact Me
+                </Link>
             </div>
+            <Logo/>
         </div>
     )
 }
